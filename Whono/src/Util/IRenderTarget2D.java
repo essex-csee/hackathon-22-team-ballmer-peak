c56eb95a5d7eb9402c10e704d09f53cc0772c081
talldie <windyvagina@gmail.com>
2022-01-14 18:23:08 +0000

    Added base interfaces and design doc

---
+++ Whono/src/Util/IRenderTarget2D.java
@@ -0,0 +1,8 @@
+package Util;
+
+import java.awt.*;
+
+public interface IRenderTarget2D
+{
+	public void draw(Graphics2D g);
+}
---
+++ Whono/src/Util/ISubscriber.java
@@ -0,0 +1,6 @@
+package Util;
+
+public interface ISubscriber
+{
+	void notify(Object notifier);
+}
