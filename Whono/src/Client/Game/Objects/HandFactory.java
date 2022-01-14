05f792cac4b3013648bf178dfe20bf3b5f5fb22d
mskorczak <mskorczak1@gmail.com>
2022-01-14 23:40:10 +0000

    Added: HandFactory + Hand

---
+++ Whono/src/Client/Game/Objects/HandFactory.java
@@ -0,0 +1,15 @@
+package Client.Game.Objects;
+
+import java.util.Collections;
+import java.util.List;
+
+public class HandFactory
+{
+
+	public static Hand createHand(int limit)
+	{
+		Hand hand = new Hand(int limit);
+
+		return deck;
+	}
+}
