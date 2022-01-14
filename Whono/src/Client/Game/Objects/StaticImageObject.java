00812fca045e186d70bc308da9ad768825bf1ad5
talldie <windyvagina@gmail.com>
2022-01-14 23:07:18 +0000

    Added: image movement testing

--- Whono/src/Client/Game/Objects/StaticImageObject.java
+++ Whono/src/Client/Game/Objects/StaticImageObject.java
@@ -1,5 +1,7 @@
 package Client.Game.Objects;
 
+import Client.Game.ClientGame;
+
 import java.awt.*;
 
 public class StaticImageObject extends GameObject
@@ -12,6 +14,24 @@ public class StaticImageObject extends GameObject
 	@Override
 	public void update(long deltaTime)
 	{
+		if(ClientGame.getInput().getKeyPressed('a'))
+		{
+			x-= deltaTime;
+		}
 
+		if(ClientGame.getInput().getKeyPressed('d'))
+		{
+			x+= deltaTime;
+		}
+
+		if(ClientGame.getInput().getKeyPressed('w'))
+		{
+			y+=deltaTime;
+		}
+
+		if(ClientGame.getInput().getKeyPressed('s'))
+		{
+			y-=deltaTime;
+		}
 	}
 }
