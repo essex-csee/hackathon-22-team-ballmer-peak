5f4aaebca1b46706babb8175d9b847e3f08092b6
talldie <windyvagina@gmail.com>
2022-01-15 22:02:04 +0000

    Added: Julia's card images

--- Whono/src/Client/Game/Objects/MainMenu/MainMenuInit.java
+++ Whono/src/Client/Game/Objects/MainMenu/MainMenuInit.java
@@ -1,7 +1,9 @@
 package Client.Game.Objects.MainMenu;
 
+import Client.Game.ClientGame;
 import Client.Game.Objects.Card;
 import Client.Game.Objects.Display.CardDisplay;
+import Client.Game.Objects.ImageManager;
 import Client.Game.Objects.InitObject;
 import Client.Game.Objects.StaticImageObject;
 
@@ -15,15 +17,14 @@ public class MainMenuInit extends InitObject
 	@Override
 	protected void setup()
 	{
+		// title
+		int titleWidth  = (int) (362f * 1.4f);
+		int titleHeight = (int) (242f * 1.4f);
+		addGameObject( new StaticImageObject(0, ClientGame.getWindowWidth()/2 - titleWidth/2, 0, titleWidth, titleHeight, ImageManager.loadImage("Whono/Assets/whonoTilt.png") ) );
+
 		addGameObject(new PlayButton());
 		addGameObject(new ExitButton());
 
-		Card c = new Card();
-
-		c.setCardColour(Card.Colour.RED);
-		c.setCardType(Card.Type.NINE);
-
-		addGameObject( new StaticImageObject(0,64,96,64,96,CardDisplay.getCardImage(c) ) );
 	}
 
 }
