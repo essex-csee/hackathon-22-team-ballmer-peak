ebdaed12adce1df8ce641a2d4066688f6d87ac0c
mskorczak <mskorczak1@gmail.com>
2022-01-15 14:20:00 +0000

    ImageManager: New Private Class + Removal by Image
    New class contains data about: image name, used count, Image object and
    image path.

--- Whono/src/Client/Game/Objects/ImageManager.java
+++ Whono/src/Client/Game/Objects/ImageManager.java
@@ -3,19 +3,39 @@ package Client.Game.Objects;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ImageManager
 {
+    private class ImageMeta
+    {
+        String name;
+        int usedCount;
+        Image image;
+        String imagePath;
+
+        ImageMeta()
+        {
+        }
+
+    }
+
+    List<ImageMeta> imageMetaList;
+
     String currentDirectory;
     ArrayList<Image> imageList;
     List<String> imageListNames;
+
 
     public ImageManager()
     {
         currentDirectory = System.getProperty("user.dir");
         imageList = new ArrayList<>();
         imageListNames = new ArrayList<String>();
+
+        imageMetaList = new ArrayList<>();
     }
 
     public String getCurrentDirectory() {
@@ -32,22 +52,53 @@ public class ImageManager
 
     public Image loadImage(String imageName)
     {
-        String imagePath = currentDirectory + imageName;
+        ImageMeta meta = new ImageMeta();
         Image out = null;
+        String imagePath = currentDirectory + imageName;
 
-        int i = imageListNames.indexOf(imageName);
-
-        if( i != -1 )
+        // Check if image already loaded
+        for(int i = 0; i < imageMetaList.size(); i++)
         {
-            out = ((Image) new ImageIcon(imagePath).getImage());
-            imageListNames.add(imageName);
-            imageList.add(out);
+            if (imageMetaList.get(i).name.equals(imageName))
+            {
+                imageMetaList.get(i).usedCount++;
+                return imageMetaList.get(i).image;
+            }
+            else // Load image
+            {
+                meta.usedCount = 1;
+                meta.name = imageName;
+                meta.imagePath = imagePath;
+                meta.image = ((Image) new ImageIcon(meta.imagePath).getImage());
+                imageMetaList.add(meta);
+                return meta.image;
+            }
         }
-        else
-        {
-            out = imageList.get(i);
-        }
+        return null;
+    }
 
+    public Image removeImage(Image delete)
+    {
+        Image out = null;
+        // Check if image already loaded
+        for(int i = 0; i < imageMetaList.size(); i++)
+        {
+            if (delete == imageMetaList.get(i).image)
+            {
+                out = imageMetaList.get(i).image;
+                // If only one image is used, remove it
+                if (imageMetaList.get(i).usedCount < 1)
+                {
+                    imageMetaList.remove(i);
+                    break;
+                }
+                // If more than one image is used, decrement usedCount
+                else
+                {
+                    imageMetaList.get(i).usedCount--;
+                }
+            }
+        }
         return out;
     }
 
