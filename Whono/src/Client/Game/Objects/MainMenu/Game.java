1742d09ac948fe74b74f8f508474f9c4fcb91efa
julia <65985506+campanula@users.noreply.github.com>
2022-01-15 16:08:23 +0000

    add rules class

---
+++ Whono/src/Client/Game/Objects/MainMenu/Game.java
@@ -0,0 +1,23 @@
+package Client.Game.Objects.MainMenu;
+
+public class Game {
+
+    public void CheckCard(){
+        // Normal cards
+        // Card must be same colour or number as last card
+
+        // Plus Cards
+        // Can always be played as +4
+        // Must match last cards colour if +2
+
+        // Skip & reverse cards
+        // Must match last cards colour
+
+        // Wildcard applies to all
+        // Can always be played
+
+
+    }
+
+
+}
